f7ccf5542a2a5b091684d5efe3e5e3dcba85e386
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-08-26 16:17:39 +0530

    Aadding code.

--- Assets/Scripts/EarthquakeHandler.cs
+++ Assets/Scripts/EarthquakeHandler.cs
@@ -8,7 +8,6 @@ public class EarthquakeHandler : MonoBehaviour
     public TMP_InputField X_Coordn;
     public TMP_InputField Z_Coordn;
     public TMP_InputField magInput;
-    public TMP_InputField radInput;
     public GameObject regionMap;
 
     public void SetCoord(float x, float z)
@@ -22,9 +21,8 @@ public class EarthquakeHandler : MonoBehaviour
         float x = float.Parse(X_Coordn.text);
         float z = float.Parse(Z_Coordn.text);
         float mag = float.Parse(magInput.text);
-        float rad = float.Parse(radInput.text);
         Debug.Log(x + " " + z);
-        regionMap.GetComponent<EarthquakeSimulator>().EathquakeColoring(new Vector3(x, 0, z), rad, mag);
+        regionMap.GetComponent<EarthquakeSimulator>().EathquakeColoring(new Vector3(x, 0, z), mag*100, mag);
     }
 
     public void Update()
